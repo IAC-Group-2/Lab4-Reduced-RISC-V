67aea244009dbf38e5964a2d0ff13bb515e0e98d
vortexisalpha <joshuaboshua250505@gmail.com>
2025-11-14 17:41:24 +0000

    for initial testing purposes, renamed mux to mux_red and kept standard mux for initial mux_tb tests

--- repo/rtl/mux.sv
+++ repo/rtl/mux.sv
@@ -1,12 +1,11 @@
 module mux #(
     DATA_WIDTH = 32
 ) (
-    input   logic [DATA_WIDTH-1:0]  regOp2, //output when ALUsrc = 0
-    input   logic [DATA_WIDTH-1:0]  ImmOp, //output when ALUsrc = 1
-    input   logic                   ALUsrc, //from control bit
-    output  logic [DATA_WIDTH-1:0]  ALUop2 //output
+    input   logic [DATA_WIDTH-1:0]  in0,
+    input   logic [DATA_WIDTH-1:0]  in1,
+    input   logic                   sel,
+    output  logic [DATA_WIDTH-1:0]  out
 );
-    //MUX outputs ImmOp when ALUsrc = 1, outputs regOp2 otherwise
-    assign ALUop2 = ALUsrc ? ImmOp : regOp2;
+    assign out = sel ? in1 : in0;
 
 endmodule
