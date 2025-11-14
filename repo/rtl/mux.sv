6ea0f266b6911406029351e18bb63e539ab6e658
Yichan Kim <yichankim@gmail.com>
2025-11-14 11:55:03 +0000

    Update naming conventions in mux.sv
    Updated variable names in mux.sv so that it is in line with the provided CPU top-level block diagram

--- repo/rtl/mux.sv
+++ repo/rtl/mux.sv
@@ -1,11 +1,12 @@
 module mux #(
     DATA_WIDTH = 32
 ) (
-    input   logic [DATA_WIDTH-1:0]  in0,
-    input   logic [DATA_WIDTH-1:0]  in1,
-    input   logic                   sel,
-    output  logic [DATA_WIDTH-1:0]  out
+    input   logic [DATA_WIDTH-1:0]  regOp2, //output when ALUsrc = 0
+    input   logic [DATA_WIDTH-1:0]  ImmOp, //output when ALUsrc = 1
+    input   logic                   ALUsrc, //from control bit
+    output  logic [DATA_WIDTH-1:0]  ALUop2 //output
 );
-    assign out = sel ? in1 : in0;
+    //MUX outputs ImmOp when ALUsrc = 1, outputs regOp2 otherwise
+    assign ALUop2 = ALUsrc ? ImmOp : regOp2;
 
 endmodule
