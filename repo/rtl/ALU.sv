18638edce2ab4965aaf765c2ccd2d58f081aaa54
Yichan Kim <yichankim@gmail.com>
2025-11-19 17:03:35 +0000

    Bug fix: ALU.sv
    Changed variable naming of inputs of ALU.sv so that it is consistent throught the project

--- repo/rtl/ALU.sv
+++ repo/rtl/ALU.sv
@@ -1,21 +1,21 @@
 module ALU #(
     DATA_WIDTH = 32
 ) (
-    input   logic [DATA_WIDTH-1:0]  in0,
+    input   logic [DATA_WIDTH-1:0]  in0 ,
     input   logic [DATA_WIDTH-1:0]  in1,
     input   logic [2:0]             ctrl,
     output  logic [DATA_WIDTH-1:0]  out,
-    output  logic                   eq,
+    output  logic                   eq
 );
 always_comb begin
     out = 0;
     eq = 0;
     case (ctrl) //codes given in lecture notes
         3'b000: out = in0 + in1; //add
-        3'b001: out = b - a; //subtract
-        3'b101: eq = a < b; //set less than
-        3'b011: out = a | b; //bitwise or
-        3'b010: out = a & b; //bitwise and
+        3'b001: out = in0 - in1; //subtract
+        3'b101: eq = in0 < in1; //set less than
+        3'b011: out = in0 | in1; //bitwise or
+        3'b010: out = in0 & in1; //bitwise and
         default:
             begin
             eq = 0;
