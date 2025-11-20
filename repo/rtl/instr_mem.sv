5e3bea505434cd4bbfa4b74febdec9c58e77eaf5
Yichan Kim <yichankim@gmail.com>
2025-11-20 20:24:15 +0000

    Bug fix: instr_mem.sv
      -Changed verilator settings so that warnings are ignored
    
     - Fixed how instruction from program.hex is loaded so that it stores the 4 byte instruction correctly

--- repo/rtl/instr_mem.sv
+++ repo/rtl/instr_mem.sv
@@ -1,14 +1,20 @@
+/* verilator lint_off UNUSED */
 module instr_mem (
     input  logic [31:0] A,
     output logic [31:0] RD
 );
 
 logic [31:0] instr_rom [255:0];
-
+logic [7:0] byte_rom [1023:0];
 initial begin
-    $readmemh("program.hex", instr_rom);
+    integer i;
+    $readmemh("../rtl/program.hex", byte_rom);
+    for (i=0; i<256; i= i+1)
+        instr_rom[i] = {byte_rom[i*4+3], byte_rom[i*4+2], byte_rom[i*4+1], byte_rom[i*4+0]};
 end
 
 assign RD = instr_rom[A[9:2]];
 
+
 endmodule
+/* verilator lint_on UNUSED */
