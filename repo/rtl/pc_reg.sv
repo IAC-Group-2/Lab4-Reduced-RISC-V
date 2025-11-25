a3487c720745affe1ccea6e5d3e1c953a7fd403a
an_thony350 <afob2@hotmail.com>
2025-11-25 13:43:34 +0000

    [update] broke down pc_reg into individual components

--- repo/rtl/pc_reg.sv
+++ repo/rtl/pc_reg.sv
@@ -3,29 +3,17 @@ module pc_reg #(
 )(
     input logic clk,
     input logic rst,
-    input logic PCsrc,
+    input logic next_pc,
     input logic [DATA_WIDTH-1:0] ImmOp,
 
-    output logic [DATA_WIDTH-1:0] PC_out
+    output logic [DATA_WIDTH-1:0] PC_o
 
 );
-
-    logic [DATA_WIDTH-1:0] branch_PC;
-    logic [DATA_WIDTH-1:0] inc_PC;
-    
-
-always_comb begin
-    branch_PC = PC_out + ImmOp;
-    inc_PC    = PC_out + 'd4;
-end
-
 
 always_ff @(posedge clk, posedge rst) 
     if (rst) 
         PC_out <= '0;
-    else if (PCsrc) 
-        PC_out <= branch_PC;
     else 
-        PC_out <= inc_PC; 
+        PC_out <= next_pc;
 
 endmodule
